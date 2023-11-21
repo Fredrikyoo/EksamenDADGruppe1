2ff8873d23c84c68c06c2e2d64bf76d06c3dd131
Fredrikyoo <Hitrik02@gmail.com>
2023-11-21 16:04:54 +0100

    DataBaseSetup

--- Assets/Scripts/LiveDataValues.cs
+++ Assets/Scripts/LiveDataValues.cs
@@ -1,3 +1,5 @@
+using System.Net;
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
@@ -34,6 +36,10 @@ public class LiveDataValues : MonoBehaviour
     public GameObject FDG1ButtonL3;
     public GameObject PDG1ButtonL3;
 
+    [Header("Other Values")]
+    public GameObject UDG1Stable;
+    public GameObject UDG1Tagname;
+
     TextMeshProUGUI DG1Text;
     TextMeshProUGUI UDG1Text;
     TextMeshProUGUI ADG1Text; 
@@ -58,9 +64,14 @@ public class LiveDataValues : MonoBehaviour
     TextMeshProUGUI COSqDG1TextL3;
     TextMeshProUGUI FDG1TextL3;
     TextMeshProUGUI PDG1TextL3; 
-     
 
-    string URL = "http://192.168.38.100/get-tunglab-data-by-tagname.php?name=DG1-LOAD&amount=1";
+    TextMeshProUGUI StableText;
+    TextMeshProUGUI TagnameText;
+     
+    public string phpURL;
+    private string url;
+
+    //http://192.168.38.100/get-tunglab-data-by-tagname.php?name=DG1-LOAD&amount=1";
     
     void Start(){
         DG1Text = DG1Button.GetComponent<TextMeshProUGUI>();
@@ -87,6 +98,25 @@ public class LiveDataValues : MonoBehaviour
         COSqDG1TextL3 = COSqDG1ButtonL3.GetComponent<TextMeshProUGUI>();
         FDG1TextL3 = FDG1ButtonL3.GetComponent<TextMeshProUGUI>();
         PDG1TextL3 = PDG1ButtonL3.GetComponent<TextMeshProUGUI>();
+
+        StableText = UDG1Stable.GetComponent<TextMeshProUGUI>();
+        TagnameText = UDG1Tagname.GetComponent<TextMeshProUGUI>();
+
+        if(Application.internetReachability==NetworkReachability.NotReachable){
+            StableText.text = "Stable test";
+            TagnameText.text = "DGX test";
+        }
+
+        string[] DG1 = {"DG1-GEN-V ", "DG1-CURR", "DG1-COS-PHI", "DG1-GEN-FRQ", "G1-AVA-POW",
+                        "G1-U-L1/L2", "G1-I-L1", "G1-COSPHI-L1", "G1-FREQ-I-L1", "val5",
+                        "G1-U-L1/L3", "G1-I-L2", "G1-COSPHI-L2", "G1-FREQ-I-L2", "val5",
+                        "G1-U-L2/L3", "G1-I-L3", "G1-COSPHI-L3", "G1-FREQ-I-L3", "val5",
+                        "DG1-CONN",""};
+        string[] DG2 = {"DG2-GEN-V ", "DG2-CURR", "DG2-COS-PHI", "DG2-GEN-FRQ", "G2-AVA-POW ",
+                        "G2-U-L1/L2", "G2-I-L1", "G2-COSPHI-L1", "G2-FREQ-I-L1", "val5",
+                        "G2-U-L1/L3", "G2-I-L2", "G2-COSPHI-L2", "G2-FREQ-I-L2", "val5",
+                        "G2-U-L2/L3", "G2-I-L3", "G2-COSPHI-L3", "G2-FREQ-I-L3", "val5",
+                        "DG2-CONN",""};
     }
 
     void Update(){
@@ -142,5 +172,18 @@ public class LiveDataValues : MonoBehaviour
             FDG1TextL3.text = "";
             PDG1TextL3.text = "";
         }
+    }
+
+    private string GetMeasurementByIndex(string measurements,int index){        //func som henter fra databsen
+        string[] parts = measurements.Split(',');                               //splitter data opp i deler i parts
+        return parts[index];                                                    //gir tilbaake valgte måling
+    }
+    private string GetMeasurementFromDatabase(GameObject Component){                                //func som henter all måling
+        url = phpURL + "?name=" + Component.name + "&amount=1";                //lager ulr
+        string response;
+        using (WebClient client = new WebClient()){
+            response = client.DownloadString(url);                              //ved å bruke konstruert url
+        }
+        return response;
     }
 }
