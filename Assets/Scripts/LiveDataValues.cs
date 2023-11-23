3659ea89e8d65746b0cdbcff7329338288cea8ee
Fredrikyoo <Hitrik02@gmail.com>
2023-11-23 15:07:44 +0100

    DG All Signlas added

--- Assets/Scripts/LiveDataValues.cs
+++ Assets/Scripts/LiveDataValues.cs
@@ -23,23 +23,10 @@ public class LiveDataValues : MonoBehaviour
     public GameObject FDG1ButtonL1;
     public GameObject PDG1ButtonL1;
 
-    [Header("Diesel Generatior L2")]
-    public GameObject UDG1ButtonL2;
-    public GameObject ADG1ButtonL2;
-    public GameObject COSqDG1ButtonL2;
-    public GameObject FDG1ButtonL2;
-    public GameObject PDG1ButtonL2;
-    
-    [Header("Diesel Generatior L3")]
-    public GameObject UDG1ButtonL3;
-    public GameObject ADG1ButtonL3;
-    public GameObject COSqDG1ButtonL3;
-    public GameObject FDG1ButtonL3;
-    public GameObject PDG1ButtonL3;
-
     [Header("Other Values")]
     public GameObject UDG1Stable;
     public GameObject UDG1Tagname;
+    public GameObject UDG1ButtonL2;
 
     TextMeshProUGUI DG1Text;
     TextMeshProUGUI UDG1Text;
@@ -53,26 +40,14 @@ public class LiveDataValues : MonoBehaviour
     TextMeshProUGUI COSqDG1TextL1;
     TextMeshProUGUI FDG1TextL1;
     TextMeshProUGUI PDG1TextL1; 
-     
-    TextMeshProUGUI UDG1TextL2;
-    TextMeshProUGUI ADG1TextL2; 
-    TextMeshProUGUI COSqDG1TextL2;
-    TextMeshProUGUI FDG1TextL2;
-    TextMeshProUGUI PDG1TextL2; 
-     
-    TextMeshProUGUI UDG1TextL3;
-    TextMeshProUGUI ADG1TextL3; 
-    TextMeshProUGUI COSqDG1TextL3;
-    TextMeshProUGUI FDG1TextL3;
-    TextMeshProUGUI PDG1TextL3; 
 
+    TextMeshProUGUI UDG1TextL2;
     TextMeshProUGUI StableText;
     TextMeshProUGUI TagnameText;
      
-    public string phpURL;
     private string url;
-
-    //http://192.168.38.100/get-tunglab-data-by-tagname.php?name=DG1-LOAD&amount=1";
+    public string[] RealDG1 = {"DG1-RUN","DG1-GEN-V","DG1-LOAD","DG1-REV-PWR","DG1-LOAD-KVAR","DG1-GEN-FRQ",
+                    "DG1-VELOC","DG1-I-L1","DG1-I-L2","DG1-I-L3","DG1-SHD"};
 
     public List<TextMeshProUGUI> textList = new List<TextMeshProUGUI>();
     
@@ -89,18 +64,7 @@ public class LiveDataValues : MonoBehaviour
         COSqDG1TextL1 = COSqDG1ButtonL1.GetComponent<TextMeshProUGUI>();
         FDG1TextL1 = FDG1ButtonL1.GetComponent<TextMeshProUGUI>();
         PDG1TextL1 = PDG1ButtonL1.GetComponent<TextMeshProUGUI>();
-
         UDG1TextL2 = UDG1ButtonL2.GetComponent<TextMeshProUGUI>();
-        ADG1TextL2 = ADG1ButtonL2.GetComponent<TextMeshProUGUI>();
-        COSqDG1TextL2 = COSqDG1ButtonL2.GetComponent<TextMeshProUGUI>();
-        FDG1TextL2 = FDG1ButtonL2.GetComponent<TextMeshProUGUI>();
-        PDG1TextL2 = PDG1ButtonL2.GetComponent<TextMeshProUGUI>();
-
-        UDG1TextL3 = UDG1ButtonL3.GetComponent<TextMeshProUGUI>();
-        ADG1TextL3 = ADG1ButtonL3.GetComponent<TextMeshProUGUI>();
-        COSqDG1TextL3 = COSqDG1ButtonL3.GetComponent<TextMeshProUGUI>();
-        FDG1TextL3 = FDG1ButtonL3.GetComponent<TextMeshProUGUI>();
-        PDG1TextL3 = PDG1ButtonL3.GetComponent<TextMeshProUGUI>();
 
         StableText = UDG1Stable.GetComponent<TextMeshProUGUI>();
         TagnameText = UDG1Tagname.GetComponent<TextMeshProUGUI>();
@@ -109,114 +73,44 @@ public class LiveDataValues : MonoBehaviour
             StableText.text = "Stable test";
             TagnameText.text = "DGX test";
         }
-
-        textList.Add(DG1Text); textList.Add(UDG1Text); textList.Add(ADG1Text); textList.Add(COSqDG1Text); textList.Add(FDG1Text); textList.Add(PDG1Text);
-
-        /*string[] Texts = {DG1Text.text, UDG1Text.text, ADG1Text.text, COSqDG1Text.text, FDG1Text.text, PDG1Text.text, 
-                        UDG1TextL1.text, ADG1TextL1.text, COSqDG1TextL1.text, FDG1TextL1.text, PDG1TextL1.text,
-                        UDG1TextL2.text, ADG1TextL2.text, COSqDG1TextL2.text, FDG1TextL2.text, PDG1TextL2.text, 
-                        UDG1TextL3.text, ADG1TextL3.text, COSqDG1TextL3.text, FDG1TextL3.text, PDG1TextL3.text,
-                        StableText.text,TagnameText.text};
-
-
-        /*string[] DG1 = {"DG1-GEN-V", "DG1-CURR", "DG1-COS-PHI", "DG1-GEN-FRQ", "G1-AVA-POW",
-                        "G1-U-L1/L2", "G1-I-L1", "G1-COSPHI-L1", "G1-FREQ-I-L1", "val5",
-                        "G1-U-L1/L3", "G1-I-L2", "G1-COSPHI-L2", "G1-FREQ-I-L2", "val5",
-                        "G1-U-L2/L3", "G1-I-L3", "G1-COSPHI-L3", "G1-FREQ-I-L3", "val5",
-                        "DG1-CONN",""};
-        string[] DG2 = {"DG2-GEN-V", "DG2-CURR", "DG2-COS-PHI", "DG2-GEN-FRQ", "G2-AVA-POW ",
-                        "G2-U-L1/L2", "G2-I-L1", "G2-COSPHI-L1", "G2-FREQ-I-L1", "val5",
-                        "G2-U-L1/L3", "G2-I-L2", "G2-COSPHI-L2", "G2-FREQ-I-L2", "val5",
-                        "G2-U-L2/L3", "G2-I-L3", "G2-COSPHI-L3", "G2-FREQ-I-L3", "val5",
-                        "DG2-CONN",""};*/
-        //to many errors.
+        // setter inn if DG1 / DG2
+        textList.Add(UDG1Text); textList.Add(ADG1Text); textList.Add(COSqDG1Text); textList.Add(FDG1Text); textList.Add(PDG1Text); textList.Add(UDG1TextL1); 
+        textList.Add(ADG1TextL1); textList.Add(COSqDG1TextL1); textList.Add(FDG1TextL1); textList.Add(PDG1TextL1); textList.Add(UDG1TextL2);
     }
 
     void Update(){
          if(Application.internetReachability==NetworkReachability.NotReachable){
-            /*DG1Text.text = "Connected to database";
-            UDG1Text.text = "400V";
-            ADG1Text.text = "14A";
-            COSqDG1Text.text = "0.8";
-            FDG1Text.text = "50HZ";
-            PDG1Text.text = "7760W";
-            
-            UDG1TextL1.text = "230V";
-            ADG1TextL1.text = "14A";
-            COSqDG1TextL1.text = "0.8";
-            FDG1TextL1.text = "50HZ";
-            PDG1TextL1.text = "2586W";
-
-            UDG1TextL2.text = "230V";
-            ADG1TextL2.text = "14A";
-            COSqDG1TextL2.text = "0.8";
-            FDG1TextL2.text = "50HZ";
-            PDG1TextL2.text = "2587W";
-
-            UDG1TextL3.text = "230V";
-            ADG1TextL3.text = "14A";
-            COSqDG1TextL3.text = "0.8";
-            FDG1TextL3.text = "50HZ";*/
-            PDG1TextL3.text = "2587W";
-
+            DG1Text.text = "Connected to database";
         } else if(Application.internetReachability==NetworkReachability.ReachableViaLocalAreaNetwork) {
             DG1Text.text = "Disconnected from database";
-            /*
-            UDG1Text.text = "";
-            ADG1Text.text = "";
+            /*for(int i = 0; i < RealDG1.Length; i += 1)
+            {
+                textList[i].text = "";
+                Debug.Log(i);
+            }
             COSqDG1Text.text = "ERROR";
-            FDG1Text.text = "";
-            PDG1Text.text = "";
-
-            UDG1TextL1.text = "";
-            ADG1TextL1.text = "";
-            COSqDG1TextL1.text = "ERROR";
-            FDG1TextL1.text = "";
-            PDG1TextL1.text = "";
-
-            UDG1TextL2.text = "";
-            ADG1TextL2.text = "";
-            COSqDG1TextL2.text = "ERROR";
-            FDG1TextL2.text = "";
-            PDG1TextL2.text = "";
-
-            UDG1TextL3.text = "";
-            ADG1TextL3.text = "";
-            COSqDG1TextL3.text = "ERROR";
-            FDG1TextL3.text = "";
-            PDG1TextL3.text = "";*/
+            COSqDG1TextL1.text = "ERROR";*/
         }
     }
     void OnTriggerEnter(Collider other){
         Debug.Log("Trigger enter");
-        string[] Texts = {DG1Text.text, UDG1Text.text, ADG1Text.text, COSqDG1Text.text, FDG1Text.text, PDG1Text.text, 
-                        UDG1TextL1.text, ADG1TextL1.text, COSqDG1TextL1.text, FDG1TextL1.text, PDG1TextL1.text,
-                        UDG1TextL2.text, ADG1TextL2.text, COSqDG1TextL2.text, FDG1TextL2.text, PDG1TextL2.text, 
-                        UDG1TextL3.text, ADG1TextL3.text, COSqDG1TextL3.text, FDG1TextL3.text, PDG1TextL3.text,
-                        StableText.text,TagnameText.text};
-        string[] RealDG1 = {"DG1-RUN","DG1-GEN-V","DG1-LOAD","DG1-REV-PWR","DG1-LOAD.KVAR","DG1-GEN-FRQ",
-                            "DG1-VELOC","DG1-I-L1","DG1-I-L2","DG1-I-L3","DG1-SHD"};
-        string DoOperation = GetValues(RealDG1, Texts);
+        string DoOperation = GetValues(RealDG1, textList);
     }
 
-    private string GetValues(string[] RealDG1, string[] Texts)
+    private string GetValues(string[] RealDG1, List<TextMeshProUGUI> Texts)
     {
-        string valboi = "5";
-        //string measurements = GetMeasurementFromDatabase(RealDG1[0]);
-        //string value = GetMeasurementByIndex(measurements,0);
-        //DG1Text.text = value;
-        //textList[0].text = value;                                                         //Doesn't work //RealDG1.Length
-        for(int i = 0; i < 5; i += 1)
+        string valboi = "5";                                                        
+        for(int i = 0; i < RealDG1.Length; i += 1)
         {
-            string measurements = GetMeasurementFromDatabase(RealDG1[i]);            //henter målinger
-            string value = GetMeasurementByIndex(measurements,0);                  // finner verdier
+            string measurements = GetMeasurementFromDatabase(RealDG1[i]);          //henter målinger
+            string value = GetMeasurementByIndex(measurements,1);                  // finner verdier
             textList[i].text = value;
         }
         Debug.Log("Operation Complete");
         return valboi;
     }
-    private string GetMeasurementFromDatabase(string system){                                //func som henter all måling
-        url = "http://192.168.38.100/get-tunglab-data-by-tagname.php?name=" + system + "&amount=1";                //lager ulr
+    private string GetMeasurementFromDatabase(string system){                                                   //func som henter all måling
+        url = "http://192.168.38.100/get-tunglab-data-by-tagname.php?name=" + system + "&amount=1";             //lager ulr
         string response;
         using (WebClient client = new WebClient()){
             response = client.DownloadString(url);                              //ved å bruke konstruert url
